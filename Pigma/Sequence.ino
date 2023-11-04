5663c8361d550e1c3c7e11d21f8ff8850e0da548
sgentiletti_meli <sergio.gentiletti@mercadolibre.com>
2023-11-03 21:45:28 -0300

    clean code

--- Pigma/Sequence.ino
+++ Pigma/Sequence.ino
@@ -19,49 +19,39 @@ void initSequence() {
 
 void sequence() {
   preHeatText();
-  timeText(totalTime);
-  temperatureText(getTemperature());
   startBlower();
   for (int i = 0; i < WAIT_FOR_HEAT_TIME; i++) {  // Wait until start heating
-    delay(1000);
-    totalTime--;
-    timeText(totalTime);
-    temperatureText(getTemperature());
+    updateDisplay();
   }
   startHeater();
   dissectText();
   for (int i = 0; i < PRE_HEAT_TIME; i++) {  // Pre-heating time
-    delay(1000);
-    timeText(totalTime);
-    totalTime--;
-    temperatureText(getTemperature());
+    updateDisplay();
   }
   isHeatingOn = true;
   for (int i = 0; i < HEAT_TIME; i++) {  // Heating time
-    delay(1000);
-    totalTime--;
-    timeText(totalTime);
-    temperatureText(getTemperature());
+    updateDisplay();
   }
   stopHeater();
   isHeatingOn = false;
   coldSystemText();
   for (int i = 0; i < WAIT_FOR_BLOW_TIME; i++) {  // Wait until stop blown
-    delay(1000);
-    totalTime--;
-    timeText(totalTime);
-    temperatureText(getTemperature());
+    updateDisplay();
   }
   stopBlower();
   for (int i = 0; i < COLD_SYSTEM_TIME; i++) {  // Wait for cold the system
-    delay(1000);
-    totalTime--;
-    timeText(totalTime);
-    temperatureText(getTemperature());
+    updateDisplay();
   }
 }
 
 void (*resetSoftware)(void) = 0;
+
+void updateDisplay() {
+  timeText(totalTime);
+  temperatureText(getTemperature());
+  delay(1000);
+  totalTime--;
+}
 
 void abortSequence() {
   stopBlower();
